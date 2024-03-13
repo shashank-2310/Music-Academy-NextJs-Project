55fed42e86679146d5b1094d66a43f2afee1be15
Shashank Kumar Gupta <91917475+shashank-2310@users.noreply.github.com>
2024-03-13 17:47:37 +0530

    FeaturedCourses, Testimonials & why choose us sections added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import TestimonialCards from "@/components/TestimonialCards";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
       <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCards />
     </main>
   );
 }
